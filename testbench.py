84468387aaf1809dc47182812082c0ed03041a5a
Durgaprasad Bhat <84469221+DurgaprasadBhat@users.noreply.github.com>
2022-04-16 11:29:59 +0530

    Update testbench.py

--- testbench.py
+++ testbench.py
@@ -2,13 +2,11 @@ import os
 
 i = 2**16 - 1 
 
-with open('Testbench.txt','w') as f:
+with open('TRACEFILE.txt','w') as f:
     for j in range(i):
-        s = '0' + str(bin(j))[2:] + ' ' + str(bin(j))[2:] + '\r\n'
+        s = '0' + (str(bin(j))[2:].zfill(16)) + ' ' + str(bin(j))[2:].zfill(16) + '\n'
         f.write(s)
 
     for j in range(i):
-        s = '1' + str(bin(j))[2:] + ' ' + str(bin(j*(2**7)))[9:] + '\r\n'
+        s = '1' + str(bin(j))[2:].zfill(16) + ' ' + str(bin(j*(2**7)))[9:].zfill(16) + '\n'
         f.write(s)
-
-
